56b08ab25e89edd7c1b9bf8bcdc68a9c19963c65
Sagarika Jaiswal <sagarikajaiswal97@gmail.com>
2023-06-21 13:57:06 +0530

    using states in class components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,42 @@
-const About = () => {
-  return (
-    <>
-      <h1>About us Page</h1>
-      <p>This is the about us page for the swiggy website clone!!!!!</p>
-    </>
-  );
-};
+import React from "react";
+import UserClass from "./UserClass";
+
+// const About = () => {
+//   return (
+//     <>
+//       <h1>About us Page</h1>
+//       <p>This is the about us page for the swiggy website clone!!!!!</p>
+//       <UserClass name={"Sagarika Jaiswal"} />
+//     </>
+//   );
+// };
+
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count1: 0,
+      count2: 0,
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <h1>{this.state.count1}</h1>
+        <button
+          onClick={() =>
+            this.setState({
+              count1: this.state.count1 + 1,
+            })
+          }
+        >
+          Increase
+        </button>
+      </>
+    );
+  }
+}
 
 export default About;
